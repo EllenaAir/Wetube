602db4dea0ccc985dd831a982440a8f889352261
EllenaAir <231000+EllenaAir@users.noreply.github.com>
2019-04-03 22:20:21 +0800

    Wetube 2-4

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 
+
 const PORT = 4000;
 
 function handleListening() {
-    console.log(`istening on: http://localhost:${port}`);
+    console.log("Listening on: http://localhost:${PORT}");
 }
+
+function handleHome(req, res){
+    res.send("Hello From home");
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile")
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
