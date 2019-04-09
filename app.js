f604a8a54ae06acaf8554d6e0f32a6a80bb6ebdc
EllenaAir <231000+EllenaAir@users.noreply.github.com>
2019-04-09 21:07:34 +0800

    Routers

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express()
-
-
-const PORT = 4000;
-
-const handleListening = () => console.log("Listening on: http://localhost:${PORT}");
-
-
-const handleHome = (req, res) => res.send("Hello From ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -21,8 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;
